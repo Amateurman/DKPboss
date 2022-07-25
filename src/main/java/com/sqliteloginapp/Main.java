6328adc0f146898d6165767e8c8f96d3bf35f284
Yogi Ibnu <87452539+Amateurman@users.noreply.github.com>
2022-07-25 10:28:20 +0700

    Update Main.java

--- src/main/java/com/sqliteloginapp/Main.java
+++ src/main/java/com/sqliteloginapp/Main.java
@@ -11,33 +11,52 @@ package com.sqliteloginapp;
  */
 import java.sql.*;
 import java.util.Scanner;
+import java.util.Base64.Encoder;
+
+import javax.crypto.ExemptionMechanismSpi;
+import org.owasp.esapi.Encoder;
+import org.owasp.esapi.codecs.XMLEntityCodec;
+import org.owasp.esapi.reference.DefaultEncoder;
 
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {			
 		
+            ESAPI.encoder().encodeForSQL(new Object(), OracleCodec(), Query);
             Connection c = null;                
             Class.forName("org.sqlite.JDBC");
             c = DriverManager.getConnection("jdbc:sqlite:accounts.db");
             c.setAutoCommit(false);
-
-            Statement stmt = null;
-            Scanner u = new Scanner(System.in);
+            // This should REALLY be validated
             System.out.println("Enter username");
-            String userName = u.nextLine();              
-            Scanner p = new Scanner(System.in); 
-            System.out.println("Enter password");
-            String passWord = p.nextLine();        
+            String userName = request.getParameter("userName");
+            try{
+                System.out.println("Enter password");
+                String passWord = request.getParameter("passWord");
+                try {
+                    Codec ORACLE_CODEC = new OracleCodec();
+                    String query = "SELECT user_id FROM user_data WHERE user_name = '"
+                    + ESAPI.encoder().encodeForSQL( ORACLE_CODEC, req.getParameter("userName"))
+                    + "' and user_password = '"
+                    + ESAPI.encoder().encodeForSQL( ORACLE_CODEC, req.getParameter("passWord")) +"'";
+                    //String query = "select * from user where username='" + userName + "' and password='" + passWord + "'";
+                    PreparedStatement pstmt= c.createStatement();
+                    pstmt.setString(1, userName);
 
-            String query = "select * from user where username='" + userName + "' and password='" + passWord + "'";
-            stmt = c.createStatement();
-
-            ResultSet res = stmt.executeQuery(query);
-            if(res.next()){
-                System.out.println("Success login, welcome "+ userName);
-            } else {
+                    ResultSet res = pstmt.executeQuery();
+                    if(res.next()){
+                        System.out.println("Success login, welcome "+ userName);
+                    } else {
+                        System.out.println("Failed login");
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Failed login");
+                }
+            } catch (SQLException se) {
                 System.out.println("Failed login");
-            }
+            }      
+
+            
                 
 	}     
 }
